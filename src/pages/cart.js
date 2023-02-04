30fc3688e9f43eb01deb571a17060911fd063bef
NahlaGalal <nahlaglal@gmail.com>
2023-02-04 19:21:34 +0200

    Add a cart UI page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react"
 import Layout from "../components/layout/layout"
-import { Card, Container, Flex, Grid, Image, Link, Text } from "theme-ui"
+import {
+  Button,
+  Card,
+  Container,
+  Flex,
+  Grid,
+  Image,
+  Link,
+  Text,
+} from "theme-ui"
 import NextLink from "next/link"
-import { formatVariantPrice } from "medusa-react"
 import { client } from "../utils/client"
 
-const Cart = ({ region }) => {
+const Cart = ({ collections }) => {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
@@ -19,105 +27,107 @@ const Cart = ({ region }) => {
       const cart = res.cart
       localStorage.setItem("cart_id", cart.id)
       setProducts(cart.items)
+      console.log(cart.items)
     }
 
     getCartItems()
   }, [])
 
+  const getCollectionName = (collectioId) => {
+    return collections.find(collection => collection.id === collectioId).title;
+  }
+
   return (
     <Layout>
       <Container variant="layout.container">
-        <Grid columns={[1, 2, 3]} gap={24} my={4}>
+        <Grid gap={24} my={4}>
           {products.map(product => (
-            <Flex
-              variant="layout.stepContainer"
-              key={product.id}
-              sx={{ justifyContent: "center" }}
-            >
-              <Card variant="container">
-                <Flex sx={{ flexDirection: "column", mt: 16 }}>
-                  <Image
-                    sx={{
-                      width: "100%",
-                      borderRadius: "4px",
-                      objectFit: "contain",
-                    }}
-                    src={product.thumbnail}
-                    alt={product.title}
-                  />
-                  <Flex sx={{ flexDirection: "column", gap: 2 }}>
-                    {product?.collection?.title && (
-                      <Text
-                        sx={{
-                          fontSize: "12px",
-                          fontWeight: 300,
-                          color: "#6B7280",
-                        }}
-                      >
-                        {product.collection.title}
-                      </Text>
-                    )}
-                    <Text
-                      sx={{
-                        fontSize: "20px",
-                        fontWeight: 600,
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
-                      }}
-                    >
-                      <NextLink href={`/${product.handle}`} passHref>
-                        <Link color="brand" sx={{ textDecoration: "none" }}>
-                          {product.title}
-                        </Link>
-                      </NextLink>
-                    </Text>
-                    <Text
-                      sx={{
-                        fontSize: "14px",
-                        fontWeight: 300,
-                        mb: "1em",
-                      }}
-                    >
-                      {/* {`${formatVariantPrice({
-                        variant: product.variants[0],
-                        region,
-                      })}`} */}
-                    </Text>
-                  </Flex>
-                </Flex>
-                <Text
+            <Card variant="container" sx={{ width: "100%" }}>
+              <Flex variant="layout.stepContainer" sx={{ gap: 3 }}>
+                <Image
                   sx={{
-                    mt: "16px",
-                    lineHeight: "24px",
-                    fontSize: "14px",
-                    fontWeight: 300,
-                    color: "#6B7280",
-                    display: "-webkit-box",
-                    WebkitLineClamp: 3,
-                    WebkitBoxOrient: "vertical",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
+                    borderRadius: "4px",
+                    objectFit: "contain",
+                    flex: 1,
+                    maxWidth: "100px",
                   }}
-                  variant="fz_s"
-                >
-                  {product.description}
-                </Text>
-              </Card>
-            </Flex>
+                  src={product.thumbnail}
+                  alt={product.title}
+                />
+
+                <Flex sx={{ flexDirection: "column", flex: 1 }}>
+                  <Text
+                    sx={{
+                      fontSize: "20px",
+                      fontWeight: 600,
+                      textOverflow: "ellipsis",
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                    }}
+                  >
+                    <NextLink
+                      href={`/${product.variant.product.handle}`}
+                      passHref
+                    >
+                      <Link color="brand" sx={{ textDecoration: "none" }}>
+                        {product.title}
+                      </Link>
+                    </NextLink>
+                  </Text>
+
+                  <Text
+                    sx={{
+                      fontSize: "12px",
+                      fontWeight: 300,
+                      color: "#6B7280",
+                    }}
+                  >
+                    {getCollectionName(product.variant.product.collection_id) || ""}
+                  </Text>
+
+                  <Text
+                    sx={{
+                      mt: "8px",
+                      lineHeight: "24px",
+                      fontSize: "14px",
+                      fontWeight: 300,
+                      color: "#6B7280",
+                      display: "-webkit-box",
+                      WebkitLineClamp: 3,
+                      WebkitBoxOrient: "vertical",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                    variant="fz_s"
+                  >
+                    {product.variant.product.description}
+                  </Text>
+
+                  <Text>Qty: {product.quantity}</Text>
+                  <Text>
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "EGP",
+                    }).format(product.total / 100)}{" "}
+                  </Text>
+                </Flex>
+              </Flex>
+            </Card>
           ))}
         </Grid>
+
+        <Button variant="cta" sx={{ mx: "auto", mb: 4, display: "block" }}>
+          Proceed to Buy
+        </Button>
       </Container>
     </Layout>
   )
 }
 
 export async function getServerSideProps() {
-  const { regions } = await client.regions.list()
+  const { collections } = await client.collections.list()
 
-  const region = regions.find(region => region.name === "Afrika")
-
-  return { props: { region } }
+  return { props: { collections } }
 }
 
 export default Cart

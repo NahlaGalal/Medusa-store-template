01a3dc1fbfd7f6651ec7065182be5e31625a291c
NahlaGalal <nahlaglal@gmail.com>
2023-02-16 00:25:06 +0200

    Resolve responsive error

--- src/components/Field/FieldSplitter.js
+++ src/components/Field/FieldSplitter.js
@@ -2,9 +2,9 @@ import React from "react"
 
 const FieldSplitter = ({ left, right }) => {
   return (
-    <div className="flex w-full flex-1">
-      <div className="mr-1 w-full">{left}</div>
-      <div className="ml-1 w-full">{right}</div>
+    <div className="flex w-full flex-1 flex-col md:flex-row">
+      <div className="md:mr-1 w-full">{left}</div>
+      <div className="md:ml-1 w-full">{right}</div>
     </div>
   )
 }
